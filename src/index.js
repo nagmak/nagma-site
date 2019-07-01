367440685bd9e5410e6de77dde8b53a6828f17ce
Nagma Kapoor <Nagma@ibms-MBP.home>
2019-07-01 13:34:39 -0400

    added breadcrumbs and menu

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import SimpleBreadcrumbs from '../src/components/breadcrumbs/breadcrumb';
+import LongMenu from '../src/components/hamburger-menu/hamburger-menu';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function IndexPage() {
+    return (
+        <div>
+            <App/>
+            <LongMenu></LongMenu>
+            <SimpleBreadcrumbs></SimpleBreadcrumbs>
+        </div>
+    );
+}
+export default IndexPage;
+
+ReactDOM.render(<IndexPage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
