bde1c657682020f7276e9ad55277a7460a257b57
nagmak <nagmakapoor@gmail.com>
2022-02-08 00:21:24 -0500

    updated employment

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -12,7 +12,7 @@ function AboutPage() {
             <div className="about-page-text">
                 <p className="about-page-para">It has been a long journey arriving here. I was born in India, grew up sailing on ships, then on shore in Singapore, Canada and USA. I identify with the experiences of being a Third Culture Kid and love to rattle on about this at random.</p>
                 <p className="about-page-para">Learning about other cultures and deep-diving into human experiences is what I enjoy the most. In my free time, I like to write poetry, short stories, read and take photos. Also a bit of a fitness junkie when stars align.</p>
-                <p className="about-page-para">Currently, I work at IBM Canada - Data Security as a Full-Stack Developer using React, Node and Go.</p>
+                <p className="about-page-para">Currently, I work at Twali as a software engineer using React, Next.js serverless. I'm curious about web3 and learning about Solidity + smart contracts.</p>
             </div>
             </header>
         </div>
